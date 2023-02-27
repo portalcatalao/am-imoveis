c7166c04f3c9df66fe4f9442b8d3fe50487626c2
andrelzm <andreluizdpaula@gmail.com>
2023-02-27 09:26:44 -0300

    att

--- src/screens/Contato/index.tsx
+++ src/screens/Contato/index.tsx
@@ -45,8 +45,8 @@ export default function Contact() {
                         <Button>Enviar</Button>
                     </Column>
                 </Form>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3790.9461316713277!2d-47.93552958511247!3d-18.16639528761619!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94a6661604976179%3A0xd8baded46c9f6c22!2sAM%20Im%C3%B3veis%20Catal%C3%A3o%20Caixa%20Aqui!5e0!3m2!1sen!2sbr!4v1677499105715!5m2!1sen!2sbr" width="600" height="450" style={{ border: 0, maxWidth: '100%' }} loading="lazy"></iframe>
             </Content>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3790.9461316713277!2d-47.93552958511247!3d-18.16639528761619!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94a6661604976179%3A0xd8baded46c9f6c22!2sAM%20Im%C3%B3veis%20Catal%C3%A3o%20Caixa%20Aqui!5e0!3m2!1sen!2sbr!4v1677499105715!5m2!1sen!2sbr" width="600" height="450" style={{border: 0, maxWidth: '100%'}} loading="lazy"></iframe>
         </Container>
     )
 }
